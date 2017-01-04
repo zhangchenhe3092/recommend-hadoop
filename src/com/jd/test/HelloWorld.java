c76f7bbbc1a97cf747fe3f2ba991ec79cb029bfb
zhangchenhe <zhangchenhe@jd.com>
2017-01-04 19:01:42 +0800

    第三次?

--- src/com/jd/test/HelloWorld.java
+++ src/com/jd/test/HelloWorld.java
@@ -2,6 +2,6 @@ package com.jd.test;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("2017-01-04 18:57:00");
+		System.out.println("2017-01-04 19:01:00");
 	}
 }
